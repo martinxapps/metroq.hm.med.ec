ae8c655404367f5268e59a6ce85c312a280ad032
Emill <lemito66@gmail.com>
2023-06-29 13:45:43 -0500

    Fecha y hora

--- src/views/tr/pedidos/formularioTerapiaRespiratoria/logic/formulario.js
+++ src/views/tr/pedidos/formularioTerapiaRespiratoria/logic/formulario.js
@@ -1,52 +1,53 @@
 
 
 export const handleCheckboxClick  = (value) => {
-    if (selectedValues.includes(value)) {
-      // Si el valor ya está en el array, lo removemos
-      selectedValues = selectedValues.filter((val) => val !== value);
-    } else {
-      // Si el valor no está en el array, lo agregamos
-      selectedValues.push(value);
-    }
+  if (selectedValues.includes(value)) {
+    // Si el valor ya está en el array, lo removemos
+    selectedValues = selectedValues.filter((val) => val !== value);
+  } else {
+    // Si el valor no está en el array, lo agregamos
+    selectedValues.push(value);
+  }
 };
 
 export const cargarFechaActual = () => {
-    const fechaActual = new Date();
-    const dia = fechaActual.getDate();
-    const mes = fechaActual.getMonth() + 1;
-    const anio = fechaActual.getFullYear();
-    const hora = fechaActual.getHours();
-    const minutos = fechaActual.getMinutes();
-    const segundos = fechaActual.getSeconds();
+  const fechaActual = new Date();
+  const dia = fechaActual.getDate();
+  const mesNormal = fechaActual.getMonth() + 1;
+  const mes = mesNormal < 10 ? `0${mesNormal}` : mesNormal;
+  const anio = fechaActual.getFullYear();
+  const hora = fechaActual.getHours();
+  const minutosNormal = fechaActual.getMinutes();
+  const minutos = minutosNormal < 10 ? `0${minutosNormal}` : minutosNormal;
 
-    const fechaFormateada = `${dia}-${mes}-${anio} ${hora}:${minutos}:${segundos}`;
-    return fechaFormateada;
-    //console.log(FormularioDeRegistro.fechaActual);
+  const fechaFormateada = `${dia}-${mes}-${anio} ${hora}:${minutos}`;
+  return fechaFormateada;
+  //console.log(FormularioDeRegistro.fechaActual);
 }
 
 export const cargarHoraActual =  () => {
-    const fechaActual = new Date();
-    const hora = fechaActual.getHours();
-    const minutos = fechaActual.getMinutes();
-    const segundos = fechaActual.getSeconds();
+  const fechaActual = new Date();
+  const hora = fechaActual.getHours();
+  const minutosNormal = fechaActual.getMinutes();
+  const minutos = minutosNormal < 10 ? `0${minutosNormal}` : minutosNormal;
 
-    const horaFormateada = `${hora}:${minutos}:${segundos}`;
-    return horaFormateada;
-    //console.log(FormularioDeRegistro.horaActual);
+  const horaFormateada = `${hora}:${minutos}`;
+  return horaFormateada;
+  //console.log(FormularioDeRegistro.horaActual);
 }
 
 export const change = (prescripcionAntes) => {
-  const newObject = {
-    PRESCRIPCIONANTES: prescripcionAntes,
-    DATOS: [],
-  };
-  Object.entries(prescripcionAntes).forEach(([nombre, seleccionado]) => {
-    if (seleccionado === true) {
-      newObject.DATOS.push({
-        nombre,
-        seleccionado,
-      });
-    }
-  });
-  return newObject;
+const newObject = {
+  PRESCRIPCIONANTES: prescripcionAntes,
+  DATOS: [],
+};
+Object.entries(prescripcionAntes).forEach(([nombre, seleccionado]) => {
+  if (seleccionado === true) {
+    newObject.DATOS.push({
+      nombre,
+      seleccionado,
+    });
+  }
+});
+return newObject;
 };
